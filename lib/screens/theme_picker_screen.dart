ca44fe53ea65bad4ae389522152e392e6441690b
minseo-1129 <m.cho24@imperial.ac.uk>
2026-09-07 23:32:21 +0900

    Remove text button press background in theme flow

--- lib/screens/theme_picker_screen.dart
+++ lib/screens/theme_picker_screen.dart
@@ -289,5 +289,5 @@ final ButtonStyle quietButtonStyle = TextButton.styleFrom(
   minimumSize: const Size(44, 44),
   tapTargetSize: MaterialTapTargetSize.shrinkWrap,
   foregroundColor: kInk,
-  overlayColor: kAccent,
+  overlayColor: Colors.transparent,
 );
